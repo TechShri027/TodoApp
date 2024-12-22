5ada44a189f6c682e9d008eb15b167e1309d2b03
TechShri027 <shrishtibansal2705@gmail.com>
2024-12-22 22:43:25 +0530

    done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let btn=document.querySelector("button")
 let inp=document.querySelector("input")
 let ul=document.querySelector("ul")
 
+
 btn.addEventListener("click", function(){
   let item=document.createElement("li")
   item.innerText=inp.value
@@ -9,19 +10,41 @@ btn.addEventListener("click", function(){
   ul.appendChild(item);
 
   let btn=document.createElement("button")
-  btn.innerText="cancel"
+  btn.innerHTML=`<i class="ri-delete-bin-6-line"></i>`
 btn.classList.add("cancel")
 item.appendChild(btn)
+
+
+  // Add the task text with a checkbox
+  let checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  item.appendChild(checkbox);
 })
 
 
 ul.addEventListener("click", function(event){
-  if(event.target.nodeName=="BUTTON"){
-    let listItem=event.target.parentElement;
+  if(event.target.closest(".cancel")){
+    let listItem=event.target.closest("li");
     listItem.remove();
+    
   }
 
 })
+
+
+
+// Add event listener for toggling strike-through on checkbox change
+ul.addEventListener("change", function (event) {
+  if (event.target.type === "checkbox") {
+    let listItem = event.target.closest("li");
+    if (event.target.checked) {
+      listItem.style.textDecoration = "line-through";
+    } else {
+      listItem.style.textDecoration = "none";
+    }
+  }
+});
+
 
 // let btns=document.querySelectorAll(".cancel")
 // for(btn of btns){
